ff7bdbdb5e9ec0f8604e20fc065cb0dbfc745052
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-08-31 03:38:16 +0100

    use router group

--- backend/main.go
+++ backend/main.go
@@ -4,9 +4,14 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/", handlers.Index)
-	r.POST("/charge", handlers.Charge)
-	r.GET("/health", handlers.Healthz)
-	r.GET("/verify", handler.Verify)
+	v1 := r.Group("/api/v1")
+	// use versioned handlers
+	{
+		v1.GET("/", handlers.Index)
+		v1.POST("/charge", handlers.Charge)
+		v1.GET("/health", handlers.Healthz)
+		v1.GET("/verify", handler.Verify)
+
+	}
 
 }
